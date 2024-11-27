d106a1bcc0d77826ffa0a6204090681f8e805c27
Lalitho14 <lalitho@DESKTOP-KEKT8EP>
2024-11-27 17:09:34 -0600

    Pagina de Inicio

---
+++ templates/footer.php
@@ -0,0 +1,23 @@
+<footer class="bg-dark text-white text-center mt-5">
+
+  <div class="container">
+    <div class="row p-3">
+      <div class="col">
+        <h5>Elaboro</h5>
+        <p>Hugo Guevara Cocolotl</p>
+        <p>Jesus Eduardo Hernandez Bravo</p>
+      </div>
+      <div class="col">
+        <h5>Contacto</h5>
+        <p>hugo.guevara@alumno.buap.mx</p>
+        <p>jesus.hernandezb@alumno.buap.mx</p>
+      </div>
+    </div>
+  </div>
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2024 Derechos Reservados:
+    <a class="text-white" href="./index.php">regalatodo.com</a>
+  </div>
+</footer>
---
+++ templates/menu.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-lg sticky-top border border-danger rounded-pill shadow-sm" style="background-color: #ffffff;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="./index.php">
+      <img src="./img/LogoRegalaTodo.png" alt="Logo" width="65" height="65" class="d-inline-block align-text-center">
+      Regala Todo
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" href="./consultaInternauta.php">Productos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="./registro.php">Registro</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="./acceso.php">Acceso</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="./contacto.php">Contacto</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
